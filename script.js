44af32a5793736f9cfdc09b7d2a7b708895f886f
mohammedsarjun <sarjunsarjunms77@gmail.com>
2025-03-08 12:06:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let homeSection = document.querySelector(".home");
+//     homeSection.style.opacity = "1";
+//     homeSection.style.transform = "translateY(0)";
+// });
